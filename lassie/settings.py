def9f9a04a8ca1db0d63689c26ea1ce84b1415cf
Greg MacWilliam <greg.macwilliam@threespot.com>
2013-06-18 00:27:07 -0400

    Adding Heroku configuration.

--- lassie/settings.py
+++ lassie/settings.py
@@ -177,3 +177,12 @@ COMPRESS_PRECOMPILERS = (
 # Tastypie settings
 API_LIMIT_PER_PAGE = 50;
 TASTYPIE_DEFAULT_FORMATS = ['json']
+
+
+# Parse database configuration from $DATABASE_URL
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
