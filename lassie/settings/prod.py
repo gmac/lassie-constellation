0e61a228ed4085d7795ee558af535674295c2c22
gmac <greg.macwilliam@threespot.com>
2013-06-18 15:46:30 -0400

    Heroku prod config.

--- lassie/settings/prod.py
+++ lassie/settings/prod.py
@@ -1,4 +1,12 @@
+import dj_database_url
 from .base import *
 
 SERVE_STATIC = True
 SERVE_MEDIA = False
+
+DATABASES = {
+    'default': dj_database_url.config(),
+}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
